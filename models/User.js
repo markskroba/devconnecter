bfc562404b2c6ab089748d524941fa140920e66e
Mark Skroba <markindo007@protonmail.com>
2021-03-20 17:31:21 -0500

    section 3 lesson 1-2 done

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = requie('mongoose');
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    passwork: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 //Connect DB
 connectDb();
 
+//Init Middleware
+app.use(express.json({ "extended": false }));
+
 app.get("/", (req, res) => {
     res.send("API Running");
 })
