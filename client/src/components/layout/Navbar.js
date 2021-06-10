fc744f67bc3e0b873486d157b96afe887dc47cda
Mark Skroba <mskroba@slu.edu>
2021-06-09 23:57:52 -0500

    register page added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <nav class="navbar bg-dark">
+        <nav className="navbar bg-dark">
         <h1>
-            <Link to="/"><i class="fas fa-code"></i> DevConnector</Link>
+            <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
         </h1>
         <ul>
             <li><Link to="profiles.html">Developers</Link></li>
