f609d382423eb1252b8713fa3a106653c33b8073
Mark Skroba <mark.skroba@slu.edu>
2023-03-06 22:34:54 -0500

    starting on the dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
     AUTH_ERROR,
     LOGIN_FAIL,
     LOGIN_SUCCESS,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 
 // LOAD USER
@@ -109,6 +110,9 @@ export const login = (email, password) => async dispatch => {
 }
 
 export const logout = () => dispatch => {
+    dispatch({
+        type: CLEAR_PROFILE
+    })
     dispatch({
         type: LOGOUT
     })
--- config/db.js
+++ config/db.js
@@ -3,14 +3,17 @@ const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, { "useNewUrlParser": true, "useCreateIndex": true });
-        console.log('MongoDB connected...')
-    } catch (err) {
-        console.error(err.message);
-        //Exit process with error
-        process.exit(1);
-    }
-}
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+		});
+		console.log("MongoDB connected...");
+	} catch (err) {
+		console.error(err.message);
+		//Exit process with error
+		process.exit(1);
+	}
+};
 
 module.exports = connectDB;
