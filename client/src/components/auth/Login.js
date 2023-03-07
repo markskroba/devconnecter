868153b7bfb7fa99871e415a875a14835a6e4ee7
Mark Skroba <mark.skroba@slu.edu>
2023-03-06 20:14:54 -0500

    client auth - login, register, logout

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth'
 import axios from 'axios';
 
-export const Login = () => {
+export const Login = ({ login, isAuthenticated }) => {
 
     const [formData, setFormData] = useState({
         email: '',
@@ -10,10 +13,16 @@ export const Login = () => {
     });
 
 
-    const {email, password} = formData;
+    const { email, password } = formData;
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
     const onSubmit = async e => {
+        e.preventDefault();
+        login(email, password);
         console.log('Success');
+    }
+
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />
     }
 
     return (
@@ -22,26 +31,35 @@ export const Login = () => {
             <p className="lead"><i className="fas fa-user"></i>Log in to your account</p>
             <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required/>
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
                 </div>
                 <div className="form-group">
-                <input
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    minLength="6"
-                    value={password} 
-                    onChange={e => onChange(e)} 
-                    required
-                />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        required
+                    />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Log in" />
             </form>
             <p className='my-1'>
                 Don't have an account? <Link to='/register'>Sign in</Link>
-            </p> 
+            </p>
         </Fragment>
     )
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+})
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,6 +1,11 @@
-import { 
+import {
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_FAIL,
+    LOGIN_SUCCESS,
+    LOGOUT
 } from '../actions/types';
 
 const initialState = {
@@ -10,13 +15,21 @@ const initialState = {
     user: null
 }
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
 
     const { type, payload } = action;
 
-    switch(type) {
+    switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: payload
+            }
 
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
                 ...state,
@@ -26,6 +39,9 @@ export default function(state = initialState, action) {
             };
 
         case REGISTER_FAIL:
+        case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT:
             localStorage.removeItem('token');
             return {
                 ...state,
