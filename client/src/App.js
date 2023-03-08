8277c4bdd0975aa20bf0f0f081f77411dcf7a15c
Mark Skroba <mark.skroba@slu.edu>
2023-03-07 19:34:54 -0500

    creating profiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,30 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Alert from './components/layout/Alert';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Alert from "./components/layout/Alert";
 
 //Redux
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import './App.css';
-import setAuthToken from './utils/setAuthToken';
-import { loadUser } from './actions/auth';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 
 if (localStorage.token) {
-	setAuthToken(localStorage.token)
+	setAuthToken(localStorage.token);
 }
 
 const App = () => {
-
 	useEffect(() => {
 		store.dispatch(loadUser());
-	}, [])
+	}, []);
 
 	return (
 		<Provider store={store}>
@@ -32,18 +32,31 @@ const App = () => {
 				<Fragment>
 					<Navbar />
 					<Route exact path="/" component={Landing} />
-					<section className='container'>
+					<section className="container">
 						<Alert />
 						<Switch>
-							<Route exact path='/register' component={Register} />
-							<Route exact path='/login' component={Login} />
-							<PrivateRoute exact path='/dashboard' component={Dashboard} />
+							<Route
+								exact
+								path="/register"
+								component={Register}
+							/>
+							<Route exact path="/login" component={Login} />
+							<PrivateRoute
+								exact
+								path="/dashboard"
+								component={Dashboard}
+							/>
+							<PrivateRoute
+								exact
+								path="/create-profile"
+								component={CreateProfile}
+							/>
 						</Switch>
 					</section>
 				</Fragment>
 			</Router>
 		</Provider>
-	)
+	);
 };
 
 export default App;
