e6714181375c82501420608ed800a32750c501c8
Mark Skroba <mark.skroba@slu.edu>
2023-03-10 11:23:56 -0500

    delete account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { getCurrentProfile } from "../../actions/profile";
+import { getCurrentProfile, deleteAccount } from "../../actions/profile";
 import { DashboardActions } from "./DashboardActions";
 import Experience from "./Experience";
 import Education from "./Education";
@@ -12,6 +12,7 @@ const Dashboard = ({
 	getCurrentProfile,
 	auth: { user },
 	profile: { profile, loading },
+	deleteAccount,
 }) => {
 	useEffect(() => {
 		getCurrentProfile();
@@ -31,6 +32,15 @@ const Dashboard = ({
 					<DashboardActions />
 					<Experience experience={profile.experience} />
 					<Education education={profile.education} />
+					<div className="my-2">
+						<button
+							className="btn btn-danger"
+							onClick={() => deleteAccount()}
+						>
+							<i className="fas fa-user-minus" /> Delete My
+							Account
+						</button>
+					</div>
 				</Fragment>
 			) : (
 				<Fragment>
@@ -50,6 +60,7 @@ Dashboard.propTypes = {
 	getCurrentProfile: PropTypes.func.isRequired,
 	auth: PropTypes.object.isRequired,
 	profile: PropTypes.object.isRequired,
+	deleteAccount: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
@@ -57,4 +68,6 @@ const mapStateToProps = (state) => ({
 	profile: state.profile,
 });
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+	Dashboard
+);
